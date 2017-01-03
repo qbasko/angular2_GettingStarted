b60b2e734b25ffda7aeda43b0c0aced3bc3cc9e9
Skoczen <jbsn@gft.com>
2017-01-03 15:22:06 +0100

    added retrieving data using http

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -18,6 +18,7 @@ export class ProductListComponent implements OnInit{
     showImage: boolean = false;
     listFilter: string;
     products: IProduct[];
+    errorMessage: string;
 
     constructor(private _productService: ProductService){
 
@@ -29,7 +30,9 @@ export class ProductListComponent implements OnInit{
     }
 
     ngOnInit(): void{
-        this.products = this._productService.getProducts();
+        this._productService.getProducts()
+            .subscribe(products => this.products = products,
+            error => this.errorMessage = <any>error);
     }
 
     onRatingClicked(message: string): void{
