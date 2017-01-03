e691209676cafd26b68360c49e4f59939ccde24a
Skoczen <jbsn@gft.com>
2017-01-03 14:15:40 +0100

    Used @Input/@Output in Nested component and added service with DI

--- app/shared/star.component.ts
+++ app/shared/star.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnChanges } from '@angular/core';
+import {Component, OnChanges, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'ai-star',
@@ -7,10 +7,16 @@ import {Component, OnChanges } from '@angular/core';
 })
 
 export class StarComponent implements OnChanges{
-    rating: number=4;
+    @Input() rating: number;
     starWidth : number;
+    @Output() ratingClicked: EventEmitter<string> = 
+        new EventEmitter<string>();
 
     ngOnChanges(): void{
         this.starWidth = this.rating *86/5;
+    }
+
+    onClick():void{
+        this.ratingClicked.emit(`The rating ${this.rating} was clicked!`);
     }
 }
